caef029eabf9185be2b453cdac11057fe14543a3
anweshasingh <72261344+anweshasingh@users.noreply.github.com>
2021-05-17 12:27:05 +0530

    Update Contestant.js

--- Contestant.js
+++ Contestant.js
@@ -1,35 +1,35 @@
 class Contestant {
-    constructor(){
-      this.index = null;
-      this.name = null;
-      this.distance = null;
-    }
-  
-    getCount(){
-      var playerCountRef = database.ref('playerCount');
-      playerCountRef.on("value",function(data){
-        playerCount = data.val();
-      })
-    }
-  
-    updateCount(count){
-      database.ref('/').update({
-        playerCount: count
-      });
-    }
-  
-    update(){
-      var playerIndex = "players/player" + this.index;
-      database.ref(playerIndex).set({
-        name:this.name,
-        distance:this.distance
-      });
-    }
-  
-    static getPlayerInfo(){
-      var playerInfo = database.ref('players');
-      playerInfo.on("value",(data)=> {
-        allPlayers = data.val()
-      })
-    }
+  constructor(){
+    this.index = null;
+    this.name = null;
+    this.distance = null;
   }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",function(data){
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfo = database.ref('players');
+    playerInfo.on("value",(data)=> {
+      allPlayers = data.val()
+    })
+  }
+}
